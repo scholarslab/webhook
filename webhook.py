af286b52f09c018d632cb4519c04c58fd8917dcc
Ammon Shepherd <ammon.uva@gmail.com>
2018-11-15 15:15:50 -0500

    Adds ability to pull from git repo

--- webhook.py
+++ webhook.py
@@ -1,9 +1,12 @@
 import os
+import git
 from flask import Flask, request, abort
 from dotenv import load_dotenv
 
 load_dotenv()
 secret_key = os.getenv('SECRET_TOKEN')
+path_to_site = "../copy-of-cool-site"
+BRANCH = 'master'
 
 app = Flask(__name__)
 
@@ -11,8 +14,11 @@ app = Flask(__name__)
 @app.route('/payload', methods=['POST'])
 def webhook():
     if request.method == 'POST':
-        if request.json['ref'] == 'refs/heads/sally':
+        if request.json['ref'] == 'refs/heads/' + BRANCH:
             print(request.headers['X-Hub-Signature'])
+            repo = git.Git(path_to_site)
+            repo.pull('origin', BRANCH)
+
             return 'Got it!', 200
     else:
         abort(400)
